dddbc89cd35e466ae43ea0499a9942dc263ffb78
SefaKoyuncu <sefakoyuncu1@outlook.com>
2023-07-16 19:05:08 +0300

    dependencies added and package schema designed

---
+++ app/src/main/java/com/sefa/countries/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sefa.countries.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sefa.countries.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
